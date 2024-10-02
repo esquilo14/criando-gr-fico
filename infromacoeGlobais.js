772dbc71adeaec75135f9964ca5bf38223377993
Vinicius <108474129+esquilo14@users.noreply.github.com>
2024-10-02 10:54:25 -0300

    02/10

--- infromacoeGlobais.js
+++ infromacoeGlobais.js
@@ -1,22 +1,20 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
-/*função assincrona, espera até que toda a 
-requisição seja executada, ou seja,
-todos os dados sejam enviadoss*/
-async function visualizarInformacoesGlobais(params) {
-    //contante que armazena uma repossta  await=espera ////fetch=faz a requisição
-    const res = await fetch(url);
-    const dados = await res.json();//espera as reposta erem convertidas em JSON
-console.log(dados);//visualizar as informações no cosole
-const paragrafo=document.createElement('p');//criar um elemento de parágrafo
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
 
-paragrafo.classList.add('graficos-container__texto');//adiciona uma clase do CSS ao parégrafo
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
-/*insere o texto "Você sabia que o mundo tem "+total_pessoas_mundo */
-paragrafo.innerHTML=`Você sabia que o mundo tem ${dados.total_pessoas_mundo}`
-
-
-
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
-visualizarInformacoesGlobais();//chama a função 
+
+vizualizarInformacoesGlobais()
