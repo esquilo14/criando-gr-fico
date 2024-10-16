3edc973ca1159300a99cc40f5e71dd8d684b0d1b
Vinicius <108474129+esquilo14@users.noreply.github.com>
2024-10-16 11:49:48 -0300

    16/10

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -2,7 +2,23 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-      
-      console.log(dados)
+    const nomeDasredes= Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+     
+    const data = [
+
+         {
+             x: nomeDasredes,
+          y: quantidadeUsuarios,
+          type:'bar'
+    }
+
+        ]
+
+        const graficos = document.createElement('div')
+      grafico.className = 'grafico'
+      document.getElementById('grafico-conteiner').appendChild(grafico)
+      plotly.newplot(grafico)
+     
   }
   
